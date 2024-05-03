24a1909816c9b2663b2eb199d21efa3ee776c513
Numberguy393 <61639194+Numberguy393@users.noreply.github.com>
2024-05-03 10:19:59 -0400

    Update interface.py
    add steerActuatorDelay to preglobal subaru outbacks

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -115,6 +115,7 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 2.74
       ret.centerToFront = ret.wheelbase * 0.5
       ret.steerRatio = 20           # learned, 14 stock
+      ret.steerActuatorDelay = 0.15
       ret.lateralTuning.init('pid')
       ret.lateralTuning.pid.kf = 0.000038
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 14., 23.], [0., 14., 23.]]
